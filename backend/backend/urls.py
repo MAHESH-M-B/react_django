a8c2917de324abe3608768adae68bc7d58bc1848
maheh <maheshmb7712@gmail.com>
2022-10-15 19:01:29 +0530

    testing

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 from backend_app import views
+from django.contrib import admin
+from django.urls import include, path
 from rest_framework import routers
+
 router=routers.DefaultRouter()
 router.register(r'tasks',views.backend_appview,'task')
 
--- backend/backend_app/serializers.py
+++ backend/backend_app/serializers.py
@@ -1,6 +1,7 @@
 from pyexpat import model
-from attr import field
+# from attr import field
 from rest_framework import serializers
+
 from .models import backend_app
 
 
